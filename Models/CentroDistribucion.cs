d6f550f018fee174231e6624f82199f0a9336587
UMGSamuel <jpatzang3@miumg.edu.gt>
2026-08-19 01:22:25 -0600

    Refactor Dashboard con modelo CentroDistribucion

---
+++ Models/CentroDistribucion.cs
@@ -0,0 +1,12 @@
+namespace TechLogistics.Models;
+
+public class CentroDistribucion
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public int Inventario { get; set; }
+
+    public bool EnLinea { get; set; }
+}
